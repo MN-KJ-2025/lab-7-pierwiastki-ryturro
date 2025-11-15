8561e2674b1a2e3be94c185b74cf7f7e728156b3
Artur Folwaczny <afolwaczny9@gmail.com>
2025-11-15 14:23:10 +0100

    lab 7

--- main.py
+++ main.py
@@ -9,10 +9,18 @@
 # =============================================================================
 import numpy as np
 import numpy.polynomial.polynomial as nppoly
+import math
 
 
 def roots_20(coef: np.ndarray) -> tuple[np.ndarray, np.ndarray] | None:
-    """Funkcja wyznaczająca miejsca zerowe wielomianu funkcją
+    if type(coef)==np.ndarray and coef.ndim==1: 
+        noise = np.random.random_sample(np.size(coef))
+        coef_add=coef+(noise*1e-10)
+        roots=nppoly.polyroots(coef_add)
+        return coef_add, roots   
+    else:
+        return None
+"""Funkcja wyznaczająca miejsca zerowe wielomianu funkcją
     `nppoly.polyroots()`, najpierw lekko zaburzając wejściowe współczynniki 
     wielomianu (N(0,1) * 1e-10).
 
@@ -25,11 +33,25 @@ def roots_20(coef: np.ndarray) -> tuple[np.ndarray, np.ndarray] | None:
             - Wektor miejsc zerowych (m,).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
-
+ 
 
 def frob_a(coef: np.ndarray) -> np.ndarray | None:
-    """Funkcja służąca do wyznaczenia macierzy Frobeniusa na podstawie
+    if type(coef)==np.ndarray and coef.ndim==1 and len(coef)>1: 
+        m=len(coef)
+        last=coef[m-1]
+        row=[]
+        for i in range (0,m-1):
+            row.append(-(coef[i]/last))   
+        print(row)
+        A=np.zeros((m-2,m-1))
+        np.fill_diagonal(A[:, 1:], 1)
+        A_new = np.vstack([A, row])
+        return A_new
+    else:
+        return None
+
+
+"""Funkcja służąca do wyznaczenia macierzy Frobeniusa na podstawie
     współczynników jej wielomianu charakterystycznego:
     w(x) = a_n*x^n + a_{n-1}*x^{n-1} + ... + a_2*x^2 + a_1*x + a_0
 
@@ -48,10 +70,16 @@ def frob_a(coef: np.ndarray) -> np.ndarray | None:
         (np.ndarray): Macierz Frobeniusa o rozmiarze (n,n).
         Jeżeli dane wejściowe są niepoprawne funkcja zwraca `None`.
     """
-    pass
-
 
 def is_nonsingular(A: np.ndarray) -> bool | None:
+    if type(A)==np.ndarray and A.ndim==2 and A.shape[0]==A.shape[1]:
+        if np.linalg.det(A) !=0:
+            return True 
+        else:
+            return False
+
+    else:
+        return None
     """Funkcja sprawdzająca czy podana macierz NIE JEST singularna. Przy
     implementacji należy pamiętać o definicji zera maszynowego.
 
